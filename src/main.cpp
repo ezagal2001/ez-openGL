d8ad38ea03bb51ad1a63eac27b1875ead08e6a74
Emmanuel Zagal <ezagal2001@gmail.com>
2025-11-09 16:12:04 -0800

    editing the ez-practice-2 branch

--- src/main.cpp
+++ src/main.cpp
@@ -150,6 +150,7 @@ void meshCreate(Mesh3D *mesh)
     //       which follow in this function. It is not necessary, but it makes
     //       the code cleaner if GPU-related functions are packed closer
     //       together versus CPU operations.
+    /* Test run edit with a different branch.
     const std::vector<GLfloat> vertexData{
         // 0 - vertex
        -0.5f, -0.5f, 0.0f, // left vertex position
@@ -165,7 +166,7 @@ void meshCreate(Mesh3D *mesh)
         0.0f, 0.0f, 1.0f, // right vertex color
 
    };
-
+*/
     // Vertex Arrays Object (VAO) setup
     // note: we can think of the VAO as a 'wrapper around' all of the Vertex
     // buffer obects; In the sense that it encapsulates all VBO state that we
